b3fbe31228b0778de7bcbda09404ed3b838ce3e7
zxcheese-dev <dokipniak@gmail.com>
2026-05-04 17:47:02 +0300

    Update tool.py

--- tool.py
+++ tool.py
@@ -1,9 +1,62 @@
 import psutil
-import time
+import os
 
-while True:
-    cpu = psutil.cpu_percent()
-    ram = psutil.virtual_memory().percent
+def script():
+    while True:
+        cpu = psutil.cpu_percent(interval=1)
+        ram = psutil.virtual_memory().percent
 
-    print(f"\rCPU: {cpu} | RAM: {ram}", end="", flush=True)
-    time.sleep(2)
+        os.system("cls" if os.name == "nt" else "clear")
+
+        if cpu >= 90:
+            print("Critical CPU usage, stop the process that uses the most CPU resources? (Y, N)")
+            inp = input()
+            inp = inp.lower()
+
+            if inp == "y":
+                for proc in psutil.process_iter():
+                    try:
+                        proc.cpu_percent(None)
+                    except (psutil.NoSuchProcess, psutil.AccessDenied):
+                        pass
+                    
+                max_proc = None
+                max_cpu = 0
+
+                for proc in psutil.process_iter(['pid', 'name']):
+                    try:
+                        cpu_use = proc.cpu_percent(None)
+
+                        if cpu_use >= max_cpu:
+                            max_cpu = cpu_use
+                            max_proc = proc
+                    except (psutil.NoSuchProcess, psutil.AccessDenied):
+                        pass
+                    
+                if max_proc:
+                    print(f"stop {max_proc.name()} ({max_proc.pid})? (Y, N)")
+
+                    while True:
+                        ans = input()
+                        ans = ans.lower()
+
+                        if ans == "y":
+                            max_proc.kill()
+                            break
+                    
+                        elif ans == "n":
+                            break
+
+                        else:
+                            print("Incorrect input (Y, N)")
+        
+        elif cpu >= 75:
+            print("Dangerous CPU usage")
+
+        if ram > 85:
+            print("Dangerous RAM usage")
+
+        print(f"CPU: {cpu:.1f} | RAM: {ram:.1f}", end="\r", flush=True)
+
+if __name__ == "__main__":
+    script()
